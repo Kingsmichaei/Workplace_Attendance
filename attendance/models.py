e2bff3974594858ba98ef59a3ab9eb1174ff2173
Kingsmichaei <kingsmichael.official@gmail.com>
2026-02-17 13:47:27 +0100

    ...

---
+++ attendance/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Attendance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    clock_in = models.TimeField(null=True, blank=True)
+    clock_out = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
